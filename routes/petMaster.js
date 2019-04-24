ef8db07aca4b27658ac17e2244e842c61af1a2e1
hqy <1094676506@qq.com>
2019-04-24 09:44:13 +0800

    hqy路由

--- routes/petMaster.js
+++ routes/petMaster.js
@@ -3,8 +3,50 @@ var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
 
-//宠物主管理
-router.post("/", async function(req, res) {
-    res.send("wsd")
+//查询所有
+router.get("/", async function (req, res) {
+    let {
+        page,
+        rows,
+        type,
+        value
+    } = req.query;
+    let option = {};
+    if (type && value) {
+        option = {
+            [type]: value
+        }
+    }
+
+    let data = await client.get("/users", {
+        page,
+        rows,
+        ...option
+    });
+    res.send(data);
+});
+//根据id查询
+router.get("/:id", async function (req, res) {
+    let id = req.params.id;
+
+    let data = await client.get("/users/" + id);
+    res.send(data);
+});
+
+router.put("/:id", async function (req, res) {
+    let {
+       
+       state
+    } = req.body;
+
+
+    // let pet = JSON.parse(req.body.pet);
+    let id = req.params.id;
+    let data = await client.put("/users/" + id, {
+       state
+    });
+
+    res.send(data);
+
 });
 module.exports = router;
